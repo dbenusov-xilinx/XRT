fba07169d387e532b927b918db90bc99f312b9e9
Daniel Benusovich <dbenusov@xilinx.com>
2022-11-10 15:49:25 -0800

    Remove extra seek call. Refactor variable declaration
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

--- src/runtime_src/core/pcie/tools/xbflash.qspi/pcidev.cpp
+++ src/runtime_src/core/pcie/tools/xbflash.qspi/pcidev.cpp
@@ -1,18 +1,6 @@
-/**
- * Copyright (C) 2020 Xilinx, Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020 Xilinx, Inc
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 
 #include <cassert>
 #include <iostream>
@@ -91,7 +79,10 @@ pci_device(const std::string& sysfs, int ubar, size_t flash_off, std::string fla
 	 
   uint32_t pcmd = 0;
   char sysfsname[20] = {0};
-  uint16_t dom = 0, b, d, f;
+  uint16_t dom = 0;
+  uint16_t b = 0;
+  uint16_t d = 0;
+  uint16_t f = 0;
   if (sscanf(sysfs.c_str(), "%hx:%hx.%hx", &b, &d, &f) < 3 &&
     sscanf(sysfs.c_str(), "%hx:%hx:%hx.%hx", &dom, &b, &d, &f) < 4)
     throw std::runtime_error("Couldn't parse entry name " + sysfs);
